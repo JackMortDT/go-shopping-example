dcd534233f9fd35b2166b747476429d4d41f5905
Jack <luis.6496@hotmail.com>
2021-08-10 19:26:28 -0500

    Remove value from slice with an index

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	power := 1000
-	fmt.Printf("default power is %d\n", power)
-  
-	name, power := "Goku", 9000
-  fmt.Printf("%s's power is over %d\n", name, power)
+	scores := []int{1, 2, 3, 4, 5}
+	scores=  removeAtIndex(scores, 2)
+	fmt.Println(scores) // [1, 2, 5, 4]
+}
+
+// No preserva el orden
+func removeAtIndex(source[]int, index int) []int {
+	lastIndex := len(source) - 1
+	// Intercambia el último valor y el valor que queremos eliminar
+	source[index], source[lastIndex] = source[lastIndex], source[index]
+	return source[:lastIndex]
 }
