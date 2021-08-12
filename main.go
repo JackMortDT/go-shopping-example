38b8b99431d7bddd87874ef1d58f05a9b1a18c87
Jack <luis.6496@hotmail.com>
2021-08-11 19:04:25 -0500

    go fmt for alins our code

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-  file, err := os.Open("a_file_to_read")
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  defer file.Close()
-  // read the file
+	file, err := os.Open("a_file_to_read")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	// read the file
 }
